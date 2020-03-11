91304d48e09aba5aa606e6d282a3a914398de961
Mas Roy <c27681@PRU.intranet.asia>
2020-03-12 01:25:02 +0700

    finish learn react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import Users from './components/Users';
+import Contact from './components/Contact';
+import NotFound from './components/Notfound';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <NavLink exact activeClassName="active" to="/"> Home </NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/users"> Users </NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/contact"> Contact </NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/users/" component={Users} />
+                <Route path="/contact"component={Contact} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(
+    routing,
+    document.getElementById('root')
+)
